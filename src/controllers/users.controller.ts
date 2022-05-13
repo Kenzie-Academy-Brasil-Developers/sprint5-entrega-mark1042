ceef8cdf93f3e6eb9707d69a4cce471eae8270b7
antonio <antoniomarcosnogueiraalmeida@gmail.com>
2022-05-13 12:16:38 -0300

    fix: minor fix

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import createUserService from "../services/user/createUser.service";
 import listUsersService from "../services/user/listUsers.service";
-import getUserById from "../services/user/getUserById.service";
+import getUserByIdService from "../services/user/getUserById.service";
 import updateUserService from "../services/user/updateUser.service";
 import deleteUserService from "../services/user/deleteUser.service";
 export default class UsersController {
@@ -31,7 +31,7 @@ export default class UsersController {
   static async show(req: Request, res: Response) {
     const { id } = req.params;
     try {
-      const user = await getUserById(id);
+      const user = await getUserByIdService(id);
       return res.status(200).json({ user });
     } catch (err) {
       if (err instanceof Error) {
